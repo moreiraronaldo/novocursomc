21531edc79767b74d354f48a2cc17c9d712c410a
moreiraronaldo <ronaldo.moreira.rm@gmail.com>
2020-07-14 19:37:49 -0300

    Criando Repository e service para categoria - Correcao

--- src/main/java/br/com/rmoreira/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/rmoreira/cursomc/resources/CategoriaResource.java
@@ -1,5 +1,6 @@
 package br.com.rmoreira.cursomc.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import br.com.rmoreira.cursomc.services.CategoriaService;
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
+	@Autowired
 	private CategoriaService categoriaService;
 
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
